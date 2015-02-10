4b519ed3cf63ac6ecaf7af7d142c0b6c34c77c86
Daniel Pollmann <danielpollmann5@hotmail.com>
2015-02-10 12:10:38 -0800

    Changed pi operation, added dot support, am now in the middle of trying to fix the history label

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,11 +12,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     
     var userIsTypingANumber = false
+    var userTypedDot = false
     
     @IBAction func appendDigit(sender: UIButton) {
     let digit = sender.currentTitle!
+
         if userIsTypingANumber {
           display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsTypingANumber = true
+        }
+    }
+    
+    @IBAction func appendDot(sender: UIButton){
+        let digit = sender.currentTitle!
+        
+        if userIsTypingANumber{
+            if !userTypedDot{
+                display.text = display.text!+digit
+                userTypedDot = true
+            }
         }else{
             display.text = digit
             userIsTypingANumber = true
@@ -26,12 +42,13 @@ class ViewController: UIViewController {
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
         if userIsTypingANumber{
+            history.text = history.text! + operation + ", "
             enter()
         }
         switch operation{
             case "×":performOperation { $0 * $1 }
             
-            case "÷":performOperation { $1 / $0 }
+            case "÷":performOperation { $1 / $0 } //cannot divide by 0!!
             
             case "+":performOperation { $0 + $1 }
             
@@ -39,11 +56,17 @@ class ViewController: UIViewController {
             
             case "√":performOperation {sqrt($0)}
             
-        case "sin":performOperation {sin($0)}
+            case "sin":performOperation {sin($0)}
         
-        case "cos": performOperation{cos($0)}
+            case "cos": performOperation{cos($0)}
             
-        case "π": performOperation{ M_PI * $0}
+            case "π":
+            
+            if operandStack.count<1{
+                display.text = "\(M_PI)"
+            }else{
+                performOperation{ M_PI * $0}
+            }
             
         default:break
         }
@@ -63,13 +86,20 @@ class ViewController: UIViewController {
         }
     }
     
-    var operandStack = Array<Double>()
+    @IBOutlet weak var history: UILabel!
     
+    var operandStack = Array<Double>()
+    var userTypedStack = Array<Double>()
    
     @IBAction func enter() {
         userIsTypingANumber = false
+        userTypedDot = false
         operandStack.append(displayValue)
         println(" operandSTack = \(operandStack)")
+    }
+    
+    func addToHistory(value : String?){
+         history.text = history.text! + value! + ", "
     }
     
     var displayValue: Double {
