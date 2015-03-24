44e94281819860aa91e7471e5ca7ee85b3df86b7
Daniel Pollmann <danielpollmann5@hotmail.com>
2015-03-24 11:44:50 -0700

    Pinching gesture is working, pan gestures not fully yet. View recognizes when to pan, but center keeps getting set to 0,0

--- CalculatorGraph/Calculator/GraphView.swift
+++ CalculatorGraph/Calculator/GraphView.swift
@@ -15,19 +15,40 @@ class GraphView: UIView {
 
     var axesDrawer = AxesDrawer()
     var scale: CGFloat = 50 {didSet {setNeedsDisplay() } }
-    var graphCenter: CGPoint{
-        get{
-            return convertPoint(center, fromView:superview)
-        }
-        set{
+    var graphCenter: CGPoint = CGPoint(){
+        didSet{
             setNeedsDisplay()
         }
     }
     
     override func drawRect(rect: CGRect) {
         // Drawing code
+        
+        graphCenter = center
+        println(" setting center" )
         axesDrawer.drawAxesInRect(rect, origin:graphCenter, pointsPerUnit: scale)
     }
     
+    func moveGraph(sender: UIPanGestureRecognizer) {
+        switch sender.state{
+        case .Ended:
+            fallthrough
+        case .Changed:
+            println("moving")
+            let translation = sender.translationInView(self)
+            graphCenter.x += translation.x
+            graphCenter.y += translation.y
+            sender.setTranslation(CGPointZero, inView:self)
+        default:
+            break
+        }
+    }
+    
+    func scale(gesture: UIPinchGestureRecognizer){
+        if gesture.state == .Changed{
+            scale *= gesture.scale
+            gesture.scale = 1
+        }
+    }
 
 }
--- CalculatorGraph/Calculator/GraphViewController.swift
+++ CalculatorGraph/Calculator/GraphViewController.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class GraphViewController: UIViewController {
-
+    @IBOutlet weak var graphView: GraphView!{
+        didSet{
+            graphView.addGestureRecognizer(UIPinchGestureRecognizer(target: graphView, action: "scale:"))
+            graphView.addGestureRecognizer(UIPanGestureRecognizer(target:graphView, action: "moveGraph:"))
+        }
+    }
+    
 }
