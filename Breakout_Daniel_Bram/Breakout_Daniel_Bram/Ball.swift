c9e0497f7278df2707f6729cce38937f95949b39
Student <student@ws-158-131.han.nl>
2015-06-11 14:47:03 +0200

    Something went wrong pushing an other project

--- Breakout_Daniel_Bram/Breakout_Daniel_Bram/Ball.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Ball.swift
-//  Breakout_Daniel_Bram
-//
-//  Created by Student on 05-06-15.
-//  Copyright (c) 2015 HAN. All rights reserved.
-//
-
-import Foundation
--- SmashTag/SmashTag/SmashTag/RecentSearchesTableViewController.swift
+++ SmashTag/SmashTag/SmashTag/RecentSearchesTableViewController.swift
@@ -50,7 +50,7 @@ class RecentSearchesTableViewController: UITableViewController {
         }
     }
     
-     //MARK: - Navigation
+    //MARK: - Navigation
     
     //In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
